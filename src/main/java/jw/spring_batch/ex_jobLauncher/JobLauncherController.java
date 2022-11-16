d6f068bce779dc46602137caee7dca5d40012746
lena1005a <lena1005a@gmail.com>
2022-11-16 13:20:28 +0900

    JobStep 사용

--- src/main/java/jw/spring_batch/ex_jobLauncher/JobLauncherController.java
+++ src/main/java/jw/spring_batch/ex_jobLauncher/JobLauncherController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
-@RestController
+//@RestController
 public class JobLauncherController {
 
     @Autowired private Job job;
--- src/main/java/jw/spring_batch/ex_variousStepBuilder/TaskletStepConfiguration.java
+++ src/main/java/jw/spring_batch/ex_variousStepBuilder/TaskletStepConfiguration.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Slf4j
 @RequiredArgsConstructor
-@Configuration
+//@Configuration
 public class TaskletStepConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
