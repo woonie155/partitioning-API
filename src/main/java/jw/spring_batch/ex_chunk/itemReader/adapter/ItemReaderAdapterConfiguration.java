11a61f7ee3261def7afadc44155c26f17afad0a6
lena1005a <lena1005a@gmail.com>
2022-11-20 02:25:55 +0900

    JsonFileItemWriter

--- src/main/java/jw/spring_batch/ex_chunk/itemReader/adapter/ItemReaderAdapterConfiguration.java
+++ src/main/java/jw/spring_batch/ex_chunk/itemReader/adapter/ItemReaderAdapterConfiguration.java
@@ -19,7 +19,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Slf4j
 @RequiredArgsConstructor
-@Configuration
+//@Configuration
 public class ItemReaderAdapterConfiguration {
 
 
--- src/main/java/jw/spring_batch/ex_chunk/itemReader/jsonItem/JsonConfiguration.java
+++ src/main/java/jw/spring_batch/ex_chunk/itemReader/jsonItem/JsonConfiguration.java
@@ -10,17 +10,20 @@ import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.json.JacksonJsonObjectMarshaller;
 import org.springframework.batch.item.json.JacksonJsonObjectReader;
+import org.springframework.batch.item.json.builder.JsonFileItemWriterBuilder;
 import org.springframework.batch.item.json.builder.JsonItemReaderBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 
 
 @Slf4j
 @RequiredArgsConstructor
-//@Configuration
+@Configuration
 public class JsonConfiguration {
 
 
@@ -55,10 +58,13 @@ public class JsonConfiguration {
 
 
     @Bean
-    public ItemWriter<User> customItemWriter(){
-        return items ->{
-            for(User u : items) log.info("write: {}", u);
-        };
+    public ItemWriter customItemWriter(){
+        return new JsonFileItemWriterBuilder()
+                .name("jsonWriter1")
+                .jsonObjectMarshaller(new JacksonJsonObjectMarshaller<>())
+                .resource(new FileSystemResource("C:\\Users\\nolan\\OneDrive\\바탕 화면\\spring_batch\\src\\main\\resources\\user5.json"))
+                .build();
+        //append(boolean): 초기화해 쓸 것인지, 뒤에 붙여 쓸 것인지 결정
     }
 
 
